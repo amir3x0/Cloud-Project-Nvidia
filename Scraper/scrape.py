d2885d77d7cc8bebe3c61c00bb6bc3ee77ff1eed
Netanel-Iyov <81302786+Netanel-Iyov@users.noreply.github.com>
2024-02-26 15:27:30 +0200

    adding scraper

--- Scraper/scrape.py
+++ Scraper/scrape.py
@@ -3,8 +3,7 @@ from bs4 import BeautifulSoup
 import argparse
 from utils.utils import *
 from firebase import firebase
-from urllib.parse import quote_plus
-import base64
+import time
 
 
 class Scraper():
@@ -15,12 +14,12 @@ class Scraper():
         base_url (str): The starting point for the web crawl.
         url_limit (int, optional): Maximum number of URLs to explore. Defaults to None (no limit).
         fb_con: An instance of a Firebase connection object.
-        url_set (set, optional): Set containing discovered URLs. Defaults to None (empty set).
+        url_dict (dict, optional): Set containing discovered URLs. Defaults to None (empty set).
 
     Attributes:
         base_url (str): The base URL for crawling.
         url_limit (int): Maximum number of URLs to explore.
-        url_set (set): Set containing discovered URLs.
+        url_dict (dict): Set containing discovered URLs.
         fb_con: Firebase connection object.
         word_dict (dict): Dictionary mapping keywords to sub-dictionaries of base64 encoded URLs and their word frequencies.
 
@@ -32,7 +31,7 @@ class Scraper():
             Checks if the URL limit has been reached.
 
         get_sub_paths(self, url: str):
-            Performs depth-first search, extracting and adding valid sub-paths within the base URL domain to the url_set. Stops if URL limit is reached.
+            Performs depth-first search, extracting and adding valid sub-paths within the base URL domain to the url_dict. Stops if URL limit is reached.
 
         build_word_dict(self):
             Iterates through URLs, extracts keywords using index_words (external function), and builds word_dict with word frequencies for each URL. Sorts URL entries within each word sub-dictionary by frequency.
@@ -44,10 +43,10 @@ class Scraper():
             Executes the entire scraping process by calling get_sub_paths, build_word_dict, and push_to_db methods sequentially.
 
     """
-    def __init__(self, base_url, fb_con, url_limit: int = None, url_set: set = None):
+    def __init__(self, base_url, fb_con, url_limit: int = None, url_dict: set = None):
         self.base_url = base_url
         self.url_limit = url_limit
-        self.url_set = url_set if url_set else set()
+        self.url_dict = url_dict if url_dict else dict()
         self.fb_con = fb_con
         self.word_dict = {}
 
@@ -75,29 +74,26 @@ class Scraper():
         Returns:
             bool: True if URL limit is reached, False otherwise.
         """
-        if self.url_limit is not None and len(self.url_set) >= self.url_limit:
+        if self.url_limit is not None and len(self.url_dict) >= self.url_limit:
             return True
 
         return False
 
     def get_sub_paths(self, url: str):
         """
-        Performs a depth-first search starting from the given URL, extracting and adding valid sub-paths within the base URL domain to the url_set. Stops if URL limit is reached.
+        Performs a depth-first search starting from the given URL, extracting and adding valid sub-paths within the base URL domain to the url_dict. Stops if URL limit is reached.
 
         Args:
             url (str): The starting URL for the sub-path search.
         """
-        # Add current URL if soup was not None
-        self.url_set.add(url)
 
         if self.check_url_limit_exceeded():
             return
 
-        # Extract all URLs mentioned in this page, if soup is none - notift and delete url from set
+        # Extract all URLs mentioned in this page, if soup is none - notify and delete url from set
         soup = self.fetch_page(url)
         if soup == None:
             print(f"Beautiful Soup for {url} was None")
-            self.url_set.remove(url)
             return
 
         discoveder_links = soup.find_all('a', href=True)
@@ -110,8 +106,8 @@ class Scraper():
             if self.base_url not in discovered_url:
                 continue
 
-            if discovered_url not in self.url_set:
-                self.url_set.add(discovered_url)
+            if discovered_url not in self.url_dict:
+                self.url_dict[discovered_url] = {'pageTitle': soup.title.text}
                 self.get_sub_paths(discovered_url)
 
     def build_word_dict(self):
@@ -121,20 +117,14 @@ class Scraper():
         Important note - word_dict that is being build with the url encoded with base64 encoding - for integrating with firebase limitations
         """
         # organize the data in the self.word_dict dictionary
-        for url in self.url_set:
+        for url in self.url_dict:
             soup = self.fetch_page(url)
             url_word_dict = index_words(soup)
             for word in url_word_dict:
-                encoded_url = base64.b64encode(url.encode()).decode()
                 self.word_dict[word] = self.word_dict.get(word, {})
-                self.word_dict[word][encoded_url] = self.word_dict[word].get(
-                    encoded_url, 0) + url_word_dict[word]
+                self.word_dict[word][url] = self.word_dict[word].get(
+                    url, 0) + url_word_dict[word]
 
-        # sort every word's url dictionary by occurence number
-        for word, url_dict in self.word_dict.items():
-            sorted_urls = sorted(
-                url_dict.items(), key=lambda x: x[1], reverse=True)
-            self.word_dict[word] = dict(sorted_urls)
 
     def push_to_db(self):
         """
@@ -145,16 +135,35 @@ class Scraper():
             Exception: If an error occurs during the database push operation.
         """
         database_node = '/test'
-        data = { word: { 'DocsIDs': url_dict } for word, url_dict in self.word_dict.items() }
-        FBconn.post(database_node, data)
+        data = [
+                    {
+                        'Term': word,
+                        'DocsIDs': [ 
+                            {
+                                'url': url, 
+                                'title': self.url_dict[url]['pageTitle'],
+                                'occuranceNumber': occurence
+                            } for url, occurence in url_dict.items()
+                        ],
+                    } for word, url_dict in self.word_dict.items()
+                ]
+        
+        db_push_start_time = time.time()
+        for document in data:
+            FBconn.post(database_node, document)
+        db_push_end_time = time.time()
+        print(f"--- Total DB push execution time: {db_push_end_time - db_push_start_time} seconds ---")
 
     def run(self):
         """
         Executes the entire scraping process by calling get_sub_paths, build_word_dict, and push_to_db methods sequentially.
         """
+        start_time = time.time()
         self.get_sub_paths(self.base_url)
         self.build_word_dict()
         self.push_to_db()
+        end_time = time.time()
+        print(f"--- Total Scraping execution time: {end_time - start_time} seconds ---")
 
 
 def get_args():
@@ -172,7 +181,8 @@ if __name__ == '__main__':
     base_url = 'https://www.nvidia.com/en-us/'
     FBconn = firebase.FirebaseApplication(
         'https://class-presentation-79426-default-rtdb.europe-west1.firebasedatabase.app/', None)
-
+    
     scraper = Scraper(base_url=base_url, fb_con=FBconn, url_limit=args.url_limit)
     scraper.run()
+
 

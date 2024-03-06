e2baa7a82eb463b7f5f321d11fcd0d6cc2653f4a
IPblood <118939745+IPblood@users.noreply.github.com>
2024-03-06 04:47:29 +0200

    minor 3

---
+++ frontend/api.js
@@ -0,0 +1,33 @@
+async function searchTerms(searchQueryString) {
+  const results = [];
+
+  // remove all non word from string
+  const words = searchQueryString.toLowerCase().match(/\w+/g);
+  console.log(words);
+
+  if (!words) {
+    return results; // Return an empty array if no words are found
+  }
+
+  // get all index from DB
+  const snapshot = await db.ref("/test").once("value");
+
+  if (snapshot.exists()) {
+    snapshot.forEach((childSnapshot) => {
+      const data = childSnapshot.val();
+
+      // Check if any word in the search query matches the Term, ignoring case
+      // Convert both to lowercase for case-insensitive comparison
+      if (data && data.Term && words.includes(data.Term.toLowerCase())) {
+        if (data.DocsIDs && data.DocsIDs.length > 0) {
+          // Push the whole data object if a match is found
+          results.push(data);
+        }
+      }
+    });
+  }
+
+  return results;
+}
+
+// document.getElement("searchButton").addEventListener("click", async () => {});

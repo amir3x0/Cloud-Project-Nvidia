be217e3d12935fc0ceda899b520e20ffed91b310
Netanel-Iyov <81302786+Netanel-Iyov@users.noreply.github.com>
2024-03-09 11:05:00 +0200

    test

--- script.js
+++ script.js
@@ -553,7 +553,9 @@ async function call(function_name) {
 }
 
 async function chat() {
+  console.log('entered chat function')
   let returned = call('chat');
+  console.log('returned value from char function', returned);
   document.getElementById("chatReturn").value = returned;
 }
 

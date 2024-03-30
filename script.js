d4e5cac5fd4a330c23d35802c4f508beb8674346
Netanel-Iyov <81302786+Netanel-Iyov@users.noreply.github.com>
2024-03-30 17:31:05 +0300

    test by id

--- script.js
+++ script.js
@@ -719,9 +719,8 @@ class Chatbox {
           openButton: document.querySelector('.chatbox__button'),
           chatBox: document.querySelector('.chatbox__support'),
           sendButton: document.querySelector('.send__button'),
-          logoutButton: document.querySelector('logoutLink')
+          logoutButton: document.querySelector('#logoutLink')
       }
-
       this.state = false;
       this.messages = [];
   }
